6e316411ae07b8d50fa118e7b3fc42d5d0ad0918
Documenter.jl <documenter@juliadocs.github.io>
2025-06-17 07:50:25 +0000

    build based on f3e2f85

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#LowDiscrepancySampling","page":"Home","title":"LowDiscrepancySampling","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LowDiscrepancySampling.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
